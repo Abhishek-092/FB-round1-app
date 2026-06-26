073b21a208325a983d300ed41cfb9bab05f9a2d1
abhishek-092 <abhishek.25004@ce.du.ac.in>
2026-06-26 16:03:04 +0530

    updating

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -99,7 +99,7 @@ export default function Navigation() {
                 </svg>
               ) : (
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="w-5 h-5">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={1.5} d="m19.5 8.25l-7.5 7.5l-7.5-7.5" />
                 </svg>
               )}
             </button>
